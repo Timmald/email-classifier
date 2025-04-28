1bae7dc18c4b678cf6da4bc029aacc82299c1107
Adam L <adam@bubai.net>
2025-04-28 00:48:30 -0400

    Organized code, created new vectors for dif cases

--- email_to_vector_V2.py
+++ email_to_vector_V2.py
@@ -3,41 +3,47 @@ import csv
 from tqdm import tqdm
 import pandas as pd
 
-# 1. Load a pretrained Sentence Transformer model
+#Load a pretrained Sentence Transformer model
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
+#Paths that can be changed
+relative_path = "newsgroups_minimal.csv"
+csv_file_path = "newsgroups_minimalV.csv"
+csv_file_path_indexed = "newsgroups_minimalIndexed.csv"
 
-def email_to_vector_v2(sentences):
-    embeddings = model.encode(sentences)
-    print(embeddings.shape)
-    similarities = model.similarity(embeddings, embeddings)
-    return(similarities)
-
-
-relative_path = "newsgroups.csv"
+#storages
 column_data = []
-column_data_2 = []
 vec_storage= []
 
-def first_ten_newsgroups(file_path):
+#converts a sentence to a vector
+def email_to_vector_v2(sentence):
+    embedding = model.encode(sentence)
+    return(embedding)
+
+#splits the file by sentence into a list
+def sentences_newsgroups(file_path):
+    global column_data
     with open(file_path, newline="") as file:
         reader = csv.reader(file, delimiter="\n")
         column_data = [row[0] for row in reader if row]
-    return(column_data)
 
-def first_ten_toVector(column_data):
-    column_data_2 = list(tqdm(column_data))
-    vec_storage = email_to_vector_v2(column_data_2)
+#turns whole list into vectors
+def convert_toVector():
+    vec_storage = email_to_vector_v2(column_data)
     df = pd.DataFrame(vec_storage)
 
     # Save to CSV
-    csv_file_path = "newsgroupsV.csv"
     df.to_csv(csv_file_path, index=False)
 
-def first_ten_Indexed(column_data):
-    df = pd.DataFrame(column_data)
-    csv_file_path1 = "newsgroupsIndexed.csv"
-    df.to_csv(csv_file_path1, index=False)
 
-#first_ten_toVector(first_ten_newsgroups(relative_path))
-first_ten_Indexed(first_ten_newsgroups(relative_path))
+
+def indexed_file():
+    df = pd.DataFrame(column_data)
+    df.to_csv(csv_file_path_indexed, index=False)
+
+
+
+
+sentences_newsgroups(relative_path)
+convert_toVector()
+indexed_file()
--- get_dataset_huggingface.py
+++ get_dataset_huggingface.py
@@ -4,14 +4,15 @@ import nltk
 from nltk.corpus import stopwords
 import re
 
-#stop words
+#sets stop words
 nltk.download("stopwords")
 stopwords_list = set(stopwords.words("english"))
 
-#loading newsgroups dataset
+#loading newsgroups dataset, takes the text from the TRAIN section of the dataset (did not use the test section) 
 ds = load_dataset("SetFit/20_newsgroups")
 vals = ds["train"]["text"]
 
+#cleans the email(single line) for new lines, random symbols, stop words,
 def clean_email(email):
     email = email.replace("\\n"," ")
     email = email.replace("\n"," ")
@@ -22,12 +23,50 @@ def clean_email(email):
     filtered_email = " ".join(word for word in words if word not in stopwords_list)
     return filtered_email
 
-df = pd.DataFrame(vals)
-csv_file_path = 'newsgroups.csv'
-with open(csv_file_path, mode='w') as file:
-    for row in vals:
-        cleaned_row = clean_email(row)
-        file.write(cleaned_row + '\n')
+#cleans email but no stop words removed this time
+def clean_email_noStopWords_removed(email):
+    email = email.replace("\\n"," ")
+    email = email.replace("\n"," ")
+    email = re.sub(r"[^\w\s@.,']", "", email).lower()
+    email = re.sub(r"\s{2,}", "", email).lower()
+    email = re.sub(r"\n{2,}", "", email).lower()
+    return email
+
+#Cleans the emails as minimally as possible
+def clean_email_minimal(email):
+    email = email.replace("\n"," ")
+    email = email.replace("\\n"," ")
+    email = re.sub(r"\s{2,}", "", email).lower()
+    email = re.sub(r"\n{2,}", "", email).lower()
+    return(email)
+
+
+#main, creates a df of vals, creates an empty csv to store the sentences after cleaning.
+#cleans each sentence, and adds it to the csv file
+def main():
+    df = pd.DataFrame(vals)
+
+    #uses clean_email, which cleans as much as possible
+    csv_file_path = 'newsgroups.csv'
+    with open(csv_file_path, mode='w') as file:
+        for row in vals:
+            cleaned_row = clean_email(row)
+            file.write(cleaned_row + '\n')
+
+    #stop words kept
+    csv_file_path = 'newsgroups_stopwords_kept.csv'
+    with open(csv_file_path, mode='w') as file:
+        for row in vals:
+            cleaned_row = clean_email_noStopWords_removed(row)
+            file.write(cleaned_row + '\n')
     
+    #minimal cleaning
+    csv_file_path = 'newsgroups_minimal.csv'
+    with open(csv_file_path, mode='w') as file:
+        for row in vals:
+            cleaned_row = clean_email_minimal(row)
+            file.write(cleaned_row + '\n')
+
+main()
 
 
