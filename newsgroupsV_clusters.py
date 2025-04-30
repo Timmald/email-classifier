b53ec37c2141a973363b502ec17075acde5aa3ba
awksrj <minhyenltv@gmail.com>
2025-04-30 18:10:55 -0400

    add some comments

--- newsgroupsV_clusters.py
+++ newsgroupsV_clusters.py
@@ -3,49 +3,48 @@ from sklearn.cluster import KMeans
 import numpy as np
 import csv
 
-with open(r"C:\Users\minhy\Downloads\newsgroupsV2.csv", 'r') as ham:
-    print("open csv file")
-    csv_reader = csv.reader(ham)
+# Load vector data from a CSV file
+with open(r"C:\Users\minhy\Downloads\newsgroupsV2.csv", 'r') as vector:
+    print("[vectors] open csv file")
+    csv_reader = csv.reader(vector)
     print("read csv file")
-    HamVectors = []    
+    vectorsArr = []    
     for item in csv_reader: 
-        HamVectors.append(item)
+        vectorsArr.append(item)
 
+# find cluster centers using KMeans with 2 clusters
 def center(vectors):
     kmeans = KMeans(n_clusters=2)
     kmeans.fit(vectors)
     return(kmeans.cluster_centers_)
 
-
+# label data points using KMeans with 3 clusters
 def label(vectors):
     kmeans = KMeans(n_clusters=3)
     kmeans.fit(vectors)
     labels = kmeans.fit_predict(vectors)
     return labels
-#test:
-# print(center(Spamlist))
 
-labeledVectors = label(HamVectors)
+#Apply the clustering to the vector data
+labeledVectors = label(vectorsArr)
 
 #fit_predict, category label
 
-with open(r"C:\Users\minhy\Downloads\newsgroupsIndexed2.csv", 'r') as ham:
-    print("open csv file")
-    csv_reader = csv.reader(ham)
+# Load email from CSV file
+with open(r"C:\Users\minhy\Downloads\newsgroupsIndexed2.csv", 'r') as email:
+    print("[emails] open csv file")
+    csv_reader = csv.reader(email)
     print("read csv file")
-    HamEnron = []
+    emailArr = []
     for item in csv_reader: 
-        HamEnron.append(item)
+        emailArr.append(item)
 
-print(len(HamEnron))
-print(len(HamVectors))
-print(len(labeledVectors))
+# Ensure the number of labels matches the number of email texts
+assert len(labeledVectors) == len(emailArr), "Length mismatch"
 
-assert len(labeledVectors) == len(HamEnron), "Length mismatch"
-
-
+# Match each email with its corresponding cluster label
 matched = []
-for label, email in zip(labeledVectors, HamEnron):
+for label, email in zip(labeledVectors, emailArr):
     matched.append((label, email[0]))
 
 for label, text in matched[:5]:
